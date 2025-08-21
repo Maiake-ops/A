7ab0ee9466f6e2c2595e14e338da287f53221aff
Maiake-ops <three.dev7083@gmail.com>
2025-08-21 18:03:29 +0700

    Update r.sh

--- r.sh
+++ r.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-cp ~/kde-config-backup/kdeglobals ~/.config/
-cp ~/kde-config-backup/plasmarc ~/.config/ 2>/dev/null
-cp ~/kde-config-backup/plasma-org.kde.plasma.desktop-appletsrc ~/.config/ 2>/dev/null
+mkdir -p ~/kde-config-backup && \
+mv ~/.config/plasma* ~/.config/kdeglobals ~/.config/plasmarc ~/.local/share/plasma* ~/kde-config-backup/ 2>/dev/null && \
+cp ~/kde-config-backup/kdeglobals ~/.config/ 2>/dev/null && \
+cp ~/kde-config-backup/plasmarc ~/.config/ 2>/dev/null && \
+cp ~/kde-config-backup/plasma-org.kde.plasma.desktop-appletsrc ~/.config/ 2>/dev/null && \
 sudo systemctl restart sddm
